7270564a4d8e511a3ad856ae6bff4cb1a0bdfbc9
Tucker <tudge669@gmail.com>
2022-11-02 16:07:31 -0500

    commit tree efe6f62fd49de83232a9ea09e616c138dbaed988

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+/*for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+  }
+*/
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+Putain Ca me baise c'est fou
+wrapGifts(gifts); */
+function writeCards(brick , name) {
+    const baguetteEtCiggarette = []
+        for (let i = 0; i<brick.length; i++) {
+        baguetteEtCiggarette.push(`Thank you, ${brick[i]}, for the wonderful ${name} gift!`)
+    }
+    return baguetteEtCiggarette
+}
+
+function countDown (num){
+    let i = 0
+    while (i <= num) {
+        console.log(num - i)
+        i++
+    }
+}
